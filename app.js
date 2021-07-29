4c9b340cbdbb200302f6db4e6686032c9b314b2b
otis wilcox <otiswilcox@gmail.com>
2021-07-29 10:34:59 -0400

    fixed a favicon.ico error

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.use(cors());
+app.get('/favicon.ico', (req, res) => res.sendStatus(200));
 const server = http.createServer(app);
 server.listen(port, hostname, () => {
     console.log(`Server is running on http://${hostname}:${port}`);
